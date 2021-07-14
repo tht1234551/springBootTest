60499937b339cb7bbf4c18c196a1743fda728896
Jung <tht1234551@gmail.com>
2021-07-14 20:44:11 +0900

    Add react

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    const name = "리액트";
+  return (
+      <>
+        <h1>{name} 안녕</h1>
+      </>
+  );
+}
+
+export default App;
